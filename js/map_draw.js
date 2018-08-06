83ca85a9769d31b61b56c0f47dc0758d8750ad51
stephenmw01 <39747878+stephenmw01@users.noreply.github.com>
2018-08-05 20:01:27 -0400

    Added map to main page.

--- js/map_draw.js
+++ js/map_draw.js
@@ -1,25 +1,29 @@
-var width = 658,
-    height = 817;
+function mapdraw() {
 
-var svg = d3.select("#mapdiv")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    var width = 658,
+        height = 817;
 
-var g = svg.append("g");
+    var svg = d3.select("#mapdiv")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-var albersProjection = d3.geoAlbers()
-    .scale(4500)
-    .rotate([0, 0])
-    .center([-3, 55.3])
-    .translate([width / 2, height / 2]);
+    var g = svg.append("g");
 
-var geoPath = d3.geoPath()
-    .projection(albersProjection);
+    var albersProjection = d3.geoAlbers()
+        .scale(4500)
+        .rotate([0, 0])
+        .center([-3, 55.3])
+        .translate([width / 2, height / 2]);
 
-g.selectAll("path")
-    .data(uk_json.features)
-    .enter()
-    .append("path")
-    .attr("fill", "#ccc")
-    .attr("d", geoPath);
+    var geoPath = d3.geoPath()
+        .projection(albersProjection);
+
+    g.selectAll("path")
+        .data(uk_json.features)
+        .enter()
+        .append("path")
+        .attr("fill", "#ccc")
+        .attr("d", geoPath);
+
+}
